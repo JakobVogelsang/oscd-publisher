24228a5705c7e19bc1027ad884eed3534c081efb
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2023-01-24 23:59:01 +0100

    feat(utils/fcda): addFCDAa and addFCDOs

--- foundation/utils/fcda.spec.ts
+++ foundation/utils/fcda.spec.ts
@@ -2,7 +2,7 @@
 import { expect } from '@open-wc/testing';
 import { isRemove, isUpdate, Update } from '@openscd/open-scd-core';
 
-import { isSubscribed, removeFCDA } from './fcda.js';
+import { addFCDAs, addFCDOs, isSubscribed, removeFCDA } from './fcda.js';
 import {
   multipleConnectionsEd1,
   multipleConnectionsEd2,
@@ -136,6 +136,179 @@ describe('Utility functions for FCDA element', () => {
         srcCBName: null,
         serviceType: null,
       });
+    });
+  });
+
+  describe('addFCDAs', () => {
+    const dataSet = new DOMParser()
+      .parseFromString(
+        `<DataSet>
+        <FCDA ldInst="someLdInst" lnClass="MMXU" lnInst="1" doName="A.phsA" daName="cVal.mag.f" fc="MX"/>
+      </DataSet>`,
+        'application/xml'
+      )
+      .querySelector('DataSet')!;
+
+    const dataSet1 = new DOMParser()
+      .parseFromString(
+        `<DataSet>
+        <FCDA ldInst="someLdInst" prefix="" lnClass="LLN0" doName="A.phsA" daName="cVal.mag.f" fc="MX"/>
+      </DataSet>`,
+        'application/xml'
+      )
+      .querySelector('DataSet')!;
+
+    const lDevice = new DOMParser()
+      .parseFromString(
+        `<LDevice inst="someLdInst"></LDevice>`,
+        'application/xml'
+      )
+      .querySelector('LDevice')!;
+
+    const anyLn = new DOMParser()
+      .parseFromString(
+        `<LN prefix="" lnClass="MMXU" inst="1"></LN>`,
+        'application/xml'
+      )
+      .querySelector('LN')!;
+
+    const anyLn1 = new DOMParser()
+      .parseFromString(`<LN lnClass="LLN0" inst=""></LN>`, 'application/xml')
+      .querySelector('LN')!;
+
+    const dO = new DOMParser()
+      .parseFromString(`<DO name="A" />`, 'application/xml')
+      .querySelector('DO')!;
+
+    const sDO = new DOMParser()
+      .parseFromString(`<SDO name="phsA" />`, 'application/xml')
+      .querySelector('SDO')!;
+
+    const dA = new DOMParser()
+      .parseFromString(`<DA name="cVal" fc="MX" />`, 'application/xml')
+      .querySelector('DA')!;
+
+    const dA1 = new DOMParser()
+      .parseFromString(`<DA name="cVal" />`, 'application/xml')
+      .querySelector('DA')!;
+
+    const bDA1 = new DOMParser()
+      .parseFromString(`<BDA name="mag" />`, 'application/xml')
+      .querySelector('BDA')!;
+
+    const bDA2 = new DOMParser()
+      .parseFromString(`<BDA name="f" />`, 'application/xml')
+      .querySelector('BDA')!;
+
+    const bDA3 = new DOMParser()
+      .parseFromString(`<BDA name="i" />`, 'application/xml')
+      .querySelector('BDA')!;
+
+    it('return empty array with missing logical node', () => {
+      const paths = [[lDevice, dO, sDO, dA, bDA1, bDA2]];
+      expect(addFCDAs(dataSet, paths).length).to.equal(0);
+    });
+
+    it('return empty array with missing logical device', () => {
+      const paths = [[anyLn, dO, sDO, dA, bDA1, bDA2]];
+      expect(addFCDAs(dataSet, paths).length).to.equal(0);
+    });
+
+    it('return empty array with missing functional constraint', () => {
+      const paths = [[lDevice, anyLn, dO, sDO, dA1, bDA1, bDA3]];
+      expect(addFCDAs(dataSet, paths).length).to.equal(0);
+    });
+
+    it('return empty array with missing data object', () => {
+      const paths = [[lDevice, anyLn, dA, bDA1, bDA3]];
+      expect(addFCDAs(dataSet, paths).length).to.equal(0);
+    });
+
+    it('return empty array with missing data attribute', () => {
+      const paths = [[lDevice, anyLn, dO, sDO]];
+      expect(addFCDAs(dataSet, paths).length).to.equal(0);
+    });
+
+    it('return empty array on duplicates', () => {
+      const paths = [[lDevice, anyLn, dO, sDO, dA, bDA1, bDA2]];
+      expect(addFCDAs(dataSet, paths).length).to.equal(0);
+    });
+
+    it('return insert array with valid new FCDA', () => {
+      const paths = [[lDevice, anyLn1, dO, sDO, dA, bDA1, bDA3]];
+      expect(addFCDAs(dataSet1, paths).length).to.equal(1);
+    });
+
+    it('allows multiple FCDA inserts', () => {
+      const paths = [
+        [lDevice, anyLn, dO, sDO, dA, bDA1, bDA2],
+        [lDevice, anyLn, dO, sDO, dA, bDA1, bDA3],
+      ];
+      expect(addFCDAs(dataSet, paths).length).to.equal(1);
+    });
+  });
+
+  describe('addFCDOs', () => {
+    const dataSet = new DOMParser()
+      .parseFromString(
+        `<DataSet>
+          <FCDA ldInst="someLdInst" lnClass="MMXU" lnInst="1" doName="A.phsA" fc="MX"/>
+        </DataSet>`,
+        'application/xml'
+      )
+      .querySelector('DataSet')!;
+
+    const lDevice = new DOMParser()
+      .parseFromString(
+        `<LDevice inst="someLdInst"></LDevice>`,
+        'application/xml'
+      )
+      .querySelector('LDevice')!;
+
+    const anyLn = new DOMParser()
+      .parseFromString(
+        `<LN prefix="" lnClass="MMXU" inst="1"></LN>`,
+        'application/xml'
+      )
+      .querySelector('LN')!;
+
+    const dO = new DOMParser()
+      .parseFromString(`<DO name="A" />`, 'application/xml')
+      .querySelector('DO')!;
+
+    const sDO = new DOMParser()
+      .parseFromString(`<SDO name="phsA" />`, 'application/xml')
+      .querySelector('SDO')!;
+
+    const fc1 = 'MX';
+    const fc2 = 'CF';
+
+    it('return empty array with missing logical node', () => {
+      const fcPaths = [{ path: [lDevice, dO, sDO], fc: fc1 }];
+      expect(addFCDOs(dataSet, fcPaths).length).to.equal(0);
+    });
+
+    it('return empty array with missing logical device', () => {
+      const fcPaths = [{ path: [anyLn, dO, sDO], fc: fc1 }];
+      expect(addFCDOs(dataSet, fcPaths).length).to.equal(0);
+    });
+
+    it('return empty array with missing data object', () => {
+      const fcPaths = [{ path: [lDevice, anyLn], fc: fc1 }];
+      expect(addFCDOs(dataSet, fcPaths).length).to.equal(0);
+    });
+
+    it('return empty array on duplicates', () => {
+      const fcPaths = [{ path: [lDevice, anyLn, dO, sDO], fc: fc1 }];
+      expect(addFCDOs(dataSet, fcPaths).length).to.equal(0);
+    });
+
+    it('allows multiple FCDA inserts', () => {
+      const fcPaths = [
+        { path: [lDevice, anyLn, dO, sDO], fc: fc1 },
+        { path: [lDevice, anyLn, dO, sDO], fc: fc2 },
+      ];
+      expect(addFCDOs(dataSet, fcPaths).length).to.equal(1);
     });
   });
 });
--- foundation/utils/fcda.ts
+++ foundation/utils/fcda.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { Remove, Update } from '@openscd/open-scd-core';
+import { Insert, Remove, Update } from '@openscd/open-scd-core';
 
 import { controlBlocks } from './controlBlocks.js';
 import {
@@ -7,6 +7,44 @@ import {
   matchExtRefFcda,
   unsubscribe,
 } from './extRef.js';
+
+function createElement(
+  doc: Document,
+  tag: string,
+  attrs: Record<string, string | null>
+): Element {
+  const element = doc.createElementNS(doc.documentElement.namespaceURI, tag);
+  Object.entries(attrs)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    .filter(([_, value]) => value !== null)
+    .forEach(([name, value]) => element.setAttribute(name, value!));
+  return element;
+}
+
+function findFcda(
+  dataSet: Element,
+  attr: {
+    ldInst: string;
+    prefix: string;
+    lnClass: string;
+    lnInst: string;
+    doName: string;
+    daName?: string;
+    fc: string;
+  }
+): Element | undefined {
+  return Array.from(dataSet.children).find(
+    fcda =>
+      fcda.tagName === 'FCDA' &&
+      fcda.getAttribute('ldInst') === attr.ldInst &&
+      (fcda.getAttribute('prefix') ?? '') === attr.prefix &&
+      (fcda.getAttribute('lnInst') ?? '') === attr.lnInst &&
+      fcda.getAttribute('lnClass') === attr.lnClass &&
+      fcda.getAttribute('doName') === attr.doName &&
+      fcda.getAttribute('daName') === (attr.daName ?? null) &&
+      fcda.getAttribute('fc') === attr.fc
+  );
+}
 
 function findFcdaSubscription(fcda: Element): Element[] {
   const doc = fcda.ownerDocument;
@@ -58,4 +96,122 @@ export function removeFCDA(fCDA: Element): (Update | Remove)[] {
 /** @returns Whether a data attribute is subscribed  */
 export function isSubscribed(fcda: Element): boolean {
   return !!findFcdaSubscription(fcda).length;
+}
+
+/** @returns Action array adding new `FCDA`s to parent [[`DataSet`]] */
+export function addFCDAs(dataSet: Element, paths: Element[][]): Insert[] {
+  const actions: Insert[] = [];
+  for (const path of paths) {
+    const anyLn = path.find(
+      section => section.tagName === 'LN' || section.tagName === 'LN0'
+    );
+    const lDevice = path.find(section => section.tagName === 'LDevice');
+
+    const ldInst = lDevice?.getAttribute('inst');
+    const prefix = anyLn?.getAttribute('prefix') ?? '';
+    const lnClass = anyLn?.getAttribute('lnClass');
+    const lnInst = anyLn?.getAttribute('inst') ?? '';
+
+    // eslint-disable-next-line no-continue
+    if (!ldInst || !lnClass) continue;
+
+    let doName = '';
+    let daName = '';
+    let fc = '';
+
+    for (const ancestor of path) {
+      // eslint-disable-next-line no-continue
+      if (!['DO', 'DA', 'SDO', 'BDA'].includes(ancestor.tagName)) continue;
+
+      const name = ancestor.getAttribute('name')!;
+
+      if (ancestor.tagName === 'DO') doName = name;
+      if (ancestor.tagName === 'SDO') doName = `${doName}.${name}`;
+      if (ancestor.tagName === 'DA') {
+        daName = name;
+        fc = ancestor.getAttribute('fc') ?? '';
+      }
+      if (ancestor.tagName === 'BDA') daName = `${daName}.${name}`;
+    }
+    // eslint-disable-next-line no-continue
+    if (!doName || !daName || !fc) continue;
+
+    const fcdaAttrs = {
+      ldInst,
+      prefix,
+      lnClass,
+      lnInst,
+      doName,
+      daName,
+      fc,
+    };
+
+    // eslint-disable-next-line no-continue
+    if (findFcda(dataSet, fcdaAttrs)) continue;
+
+    actions.push({
+      parent: dataSet,
+      node: createElement(dataSet.ownerDocument, 'FCDA', fcdaAttrs),
+      reference: null,
+    });
+  }
+
+  return actions;
+}
+
+/** @returns Action array adding new `FCDA`s to parent [[`DataSet`]] */
+export function addFCDOs(
+  dataSet: Element,
+  fcPaths: { path: Element[]; fc: string }[]
+): Insert[] {
+  const actions: Insert[] = [];
+  for (const fcPath of fcPaths) {
+    const anyLn = fcPath.path.find(
+      section => section.tagName === 'LN' || section.tagName === 'LN0'
+    );
+    const lDevice = fcPath.path.find(section => section.tagName === 'LDevice');
+
+    const ldInst = lDevice?.getAttribute('inst');
+    const prefix = anyLn?.getAttribute('prefix') ?? '';
+    const lnClass = anyLn?.getAttribute('lnClass');
+    const lnInst = anyLn?.getAttribute('inst') ?? '';
+
+    // eslint-disable-next-line no-continue
+    if (!ldInst || !lnClass) continue;
+
+    let doName = '';
+    const { fc } = fcPath;
+
+    for (const ancestor of fcPath.path) {
+      // eslint-disable-next-line no-continue
+      if (!['DO', 'SDO'].includes(ancestor.tagName)) continue;
+
+      const name = ancestor.getAttribute('name')!;
+
+      if (ancestor.tagName === 'DO') doName = name;
+      if (ancestor.tagName === 'SDO') doName = `${doName}.${name}`;
+    }
+    // eslint-disable-next-line no-continue
+    if (!doName) continue;
+
+    const fcdaAttrs = {
+      ldInst,
+      prefix,
+      lnClass,
+      lnInst,
+      doName,
+      fc,
+    };
+
+    // eslint-disable-next-line no-continue
+    if (findFcda(dataSet, fcdaAttrs)) continue;
+
+    actions.push({
+      parent: dataSet,
+      node: createElement(dataSet.ownerDocument, 'FCDA', fcdaAttrs),
+      reference: null,
+    });
+  }
+
+  return actions;
 }
